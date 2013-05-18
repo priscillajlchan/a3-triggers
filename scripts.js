649b4de32a3ea6d5ffbf40a08c07af09244a8c01
Priscilla <priscillachan__@hotmail.com>
2013-05-17 19:29:17 -0700

    :even Psuedo Selector
    homework done!
    (+ a side note to self!)

--- scripts.js
+++ scripts.js
@@ -4,3 +4,6 @@ $(document).ready(function() {
 	$("#vacations > li:even").text("Vancouver,BC!!");
 });
 
+// just a side note :D
+// even will select numbers like 2,4,6 etc. 
+// odd also works! and will select the 1,3,5 etc. 
